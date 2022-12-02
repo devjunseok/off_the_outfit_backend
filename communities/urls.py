7dec23f0f699cd8eb6a39e61553d3de829ec040b
hanmariyang <hanmari.code@gmail.com>
2022-12-02 10:19:11 +0900

    [추가] 게시글 작성 views / urls / serializers 추가

--- communities/urls.py
+++ communities/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from communities import views
 
 urlpatterns = [
-    path('', views.AllFeedView.as_view(), name ='feed_list' ), # 게시글 전체보기
+    path('', views.ArticlesFeedView.as_view(), name ='feed_list' ), # 게시글 전체보기
 ]
