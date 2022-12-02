7ae34aec17640f8fb0f3004747348b72b1e73297
devjunseok <wnslgood58@naver.com>
2022-12-02 09:21:54 +0900

    [추가] 회원탈퇴 기눙 추가

--- users/views.py
+++ users/views.py
@@ -22,12 +22,14 @@ class UserView(APIView):
         serializer = UserProfileSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+    
     def post(self, request):  # 회원가입
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({"message":"가입완료!"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
     
     def put(self, request): # 회원정보 수정
         user = get_object_or_404(User, id=request.user.id)
@@ -36,11 +38,17 @@ class UserView(APIView):
             if serializer.is_valid():
                 serializer.save()
                 return Response({"message":"변경되었습니다!"}, status=status.HTTP_200_OK)
-        
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({"message":"권한이 없습니다!"}, status=status.HTTP_403_FORBIDDEN)
 
+
+    def delete(self, request): # 회원탈퇴
+        if request.user.is_authenticated:
+            request.user.delete()
+            return Response({"message":"탈퇴되었습니다!"}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({"message":"권한이 없습니다!"}, status=status.HTTP_403_FORBIDDEN)
 
 class CustomTokenObtainPairView(TokenObtainPairView): # jwt payload 커스텀
     serializer_class = CustomTokenObtainPairSerializer
