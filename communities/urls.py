83dc2b7fdc2d7100714d578bb927b1502441e5e7
poro625 <poro625@naver.com>
2022-12-02 20:42:19 +0900

    [추가] 게시글 태그 기능

--- communities/urls.py
+++ communities/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from communities import views
 
 urlpatterns = [
-    path('', views.ArticlesFeedView.as_view(), name ='feed_list' ),                                # 게시글 전체보기, 등록
-    path('<int:feed_id>/', views.CommunitySearchView.as_view(), name ='feed_detail_view' ),     # 게시글 상세조회, 수정, 삭제
-    path('search/', views.CommunitySearchView.as_view(), name = 'communities_search_view'),  # 유저 검색 url
+    path('', views.ArticlesFeedView.as_view(), name ='feed_list' ),                          # 게시글 전체보기, 등록
+    path('<int:feed_id>/', views.CommunitySearchView.as_view(), name ='feed_detail_view' ),  # 게시글 상세조회, 수정, 삭제
+    path('search/', views.CommunitySearchView.as_view(), name = 'communities_search_view'),  # 게시글 검색
+    path('tags/', views.CommunityTagView.as_view(), name = 'tags_view'),                  # 게시글 태그
 ]
