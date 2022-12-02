48e6fb096bc224d13f1b070f8aae9e5e37f41ea9
hanmariyang <hanmari.code@gmail.com>
2022-12-03 03:43:19 +0900

    [추가] products app 어드민 연결

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from products.models import *
 
 # Register your models here.
+admin.site.register(Products)
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(ProductCategoryRelation)
