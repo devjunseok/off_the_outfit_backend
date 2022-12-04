b8c37ca2b8e6177944bddb5e9bec565b00e07092
노우석 <nhowooseok@gmail.com>
2022-12-04 15:41:13 +0900

    [추가]오래된 유저 정보 삭제 기능 추가

--- users/management/commands/Delete_old_user.py
+++ users/management/commands/Delete_old_user.py
@@ -4,8 +4,8 @@ from users.models import User
 from datetime import datetime, timedelta
 
 class Command(BaseCommand):
-    help = '10일보다 오래된 objects를 지운다.'
+    help = '365일보다 오래된 objects를 지운다.'
 
     def handle(self, *args, **options):
-        User.objects.filter(creating_date__lte=datetime.now()-timedelta(days=10)).delete()
-        self.stdout.write('10일보다 오래된 유저 오브젝트를 제거합니다')
+        User.objects.filter(last_login__lte=datetime.now()-timedelta(days=365)).delete()
+        self.stdout.write('365일보다 오래된 유저 오브젝트를 제거합니다')
