4cf2bf9abcd722dbc8bb9bd49a7552d75d78e21b
hanmariyang <hanmari.code@gmail.com>
2022-12-04 22:41:41 +0900

    [추가] Post, Reply 모델링

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
 
 class Brand(models.Model):
     brand_name_kr = models.CharField('브랜드명_국문', max_length=50)
@@ -11,7 +11,7 @@ class Brand(models.Model):
         return str(self.brand_name_kr)
 
 
-class Products(models.Model):
+class Product(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     product_number = models.IntegerField('상품번호')
     product_name = models.CharField('상품명', max_length=50)
@@ -37,7 +37,42 @@ class Category(models.Model):
 
 
 class ProductCategoryRelation(models.Model):
-    products = models.ForeignKey(Products, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    POST_TYPE = (
+        ('REVIEW', '후기(review)'),
+        ('Q&A', '문의(Q&A)'),
+    )
     
+    POST_STATUS = (
+        ('NOMAL', '일반'),
+        ('UNANSWERED', '답변 예정'),
+        ('DONE', '답변 완료'),
+    )
     
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    post_type = models.CharField('종류', choices=POST_TYPE, max_length=20, default=POST_TYPE[0][0])
+    post_status = models.CharField('상태', choices=POST_STATUS, max_length=20, default=POST_STATUS[0][0])
+    image = models.ImageField('이미지', blank=True, upload_to="review_images/", null=True)
+    content = models.TextField('내용')
+    rating = models.IntegerField('평점', null=True, blank=True)
+    like = models.ManyToManyField(User, related_name='like_reviews', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.content)
+    
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, blank=True, related_name="posts")
+    reply = models.TextField('댓글')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.reply)
