64c51bf481d3d93d0b7ec278098516877a719c7c
hanmariyang <hanmari.code@gmail.com>
2022-12-04 21:10:40 +0900

    [추가] Products, Brand, Category 기본 시리얼라이저 추가

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,23 @@
 from rest_framework import serializers
-from products.models import Brand, Products
+from products.models import Brand, Products, Category
 
 
-class ProductsSerializer(serializers.ModelSerializer):
+class ProductsSerializer(serializers.ModelSerializer): # 상품 정보 전체 조회,
     
     class Meta:
         model = Products
+        fields = '__all__'
+        
+
+class BrandSerializer(serializers.ModelSerializer): # 브랜드 정보 전체 조회,
+    
+    class Meta:
+        model = Brand
+        fields = '__all__'
+        
+
+class CategorySerializer(serializers.ModelSerializer): # 카테고리 정보 전체 조회,
+    
+    class Meta:
+        model = Category
         fields = '__all__'
