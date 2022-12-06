6d23a06c9112cff4c143a930993cd26963c0f6a8
devjunseok <wnslgood58@naver.com>
2022-12-06 17:36:24 +0900

    [추가] 관리자 권한 유저의 유저 관리 기능 추가

--- manager/urls.py
+++ manager/urls.py
@@ -4,7 +4,10 @@ from manager import views
 urlpatterns = [
 
     path('reportfeed/', views.ReportFeedView.as_view(), name ='report_feed_list' ), # 신고게시글 열람
-    path('reportfeed/<int:feed_id>/', views.ReportFeedDetailView.as_view(), name ='report_feed_detail'), # 관리자 페이지 기능
+    path('feedmanage/<int:feed_id>/', views.FeedManageView.as_view(), name ='feed_manage'), # 게시글 관리 기능
+    path('usermanage/', views.UserManageView.as_view(), name ='user_manage'), # 관리자 유저 목록 열람 기능
+    path('usermanage/<int:user_id>/', views.UserManageDetailView.as_view(), name ='user_manage'), # 관리자 유저 관리 기능
+ 
  
 ]
 
--- manager/views.py
+++ manager/views.py
@@ -3,7 +3,7 @@ from communities.serializers import FeedListSerializer
 from communities.models import Feed 
 
 from users.models import User
-
+from users.serializers import UserSerializer, CustomTokenObtainPairSerializer , UserProfileSerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -24,7 +24,7 @@ class ReportFeedView(APIView): # 일정횟수 신고당한 게시글 열람 View
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response({"message":"권한이 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
-class ReportFeedDetailView(APIView): # 신고당한 게시글 관리 View(관리자 권한)
+class FeedManageView(APIView): # 관리자 권한 게시글 관리 View
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication]
     
@@ -43,4 +43,33 @@ class ReportFeedDetailView(APIView): # 신고당한 게시글 관리 View(관리
         if user.roles == 'ROLE_SUPER':
             feed.delete()
             return Response({"message":"게시글 삭제했습니다!"}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"message":"권한이 없습니다!"}, status=status.HTTP_400_BAD_REQUEST)
+
+class UserManageView(APIView): # 관리자 권한 유저 목록 열람 View
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    
+    def get(self, request): # 회원정보 전체 보기
+        user = get_object_or_404(User, id=request.user.id)
+        all_user = User.objects.all()
+        serializer = UserProfileSerializer(all_user, many=True)
+        if user.roles == 'ROLE_SUPER':
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"message":"권한이 없습니다!"}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class UserManageDetailView(APIView): # 관리자 권한 유저 목록 열람 View
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+        
+
+    def delete(self, request, user_id): # 신고당한 게시글 삭제
+        user = get_object_or_404(User, id=request.user.id)
+        all_user = get_object_or_404(User, id=user_id)
+        if user.roles == 'ROLE_SUPER':
+            if user.id == all_user.id:
+                return Response({"message":"본인을 삭제할 수 없습니다!"}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                all_user.delete()
+                return Response({"message":"회원을 삭제했습니다!"}, status=status.HTTP_204_NO_CONTENT)
         return Response({"message":"권한이 없습니다!"}, status=status.HTTP_400_BAD_REQUEST)
