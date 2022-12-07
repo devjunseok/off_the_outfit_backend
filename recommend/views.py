abae416301d84f7c51024fdd609d6357c946807d
devjunseok <wnslgood58@naver.com>
2022-12-07 23:08:12 +0900

    [수정] recommend views.py if문 수정

--- recommend/views.py
+++ recommend/views.py
@@ -13,26 +13,18 @@ class ProductRecommendView(APIView):
     def post(self, request):
         now_date = datetime.today().strftime("%m.%d.")
         serializer = RegionSerializer(data=request.data)
+    
+        user_region =  Weather.objects.filter(city=request.data["city"]).values()[1]
+        print(user_region)
+        user_region=user_region["city"]
+        user_day_region = Weather.objects.filter(Q(city=user_region) & Q(day_date = now_date)).values()[0]
+        user_temperature = user_day_region["day_temperature"]
         
-
-
-        if request.data["city"] == "서울특별시":
-            print("if문통과")
-            user_region =  Weather.objects.filter(city=request.data["city"]).values()[0]
-            
-            user_region=user_region["city"]
-            user_day_region = Weather.objects.filter(Q(city=user_region) & Q(day_date = now_date)).values()[0]
-            user_temperature = user_day_region["day_temperature"]
-
-            
-            if user_temperature < 30:
-                
-                if serializer.is_valid():
-                    print("hi")
-                    product = Product.objects.filter(Q(category__gte=19) & Q(category__lte=36))
-                    serializer = ProductSerializer(product, many=True)
-                    print(serializer)
-                return Response(serializer.data)
-            return Response("틀렸음")
-        return Response("일단틀림")
-        
+        if user_temperature < 30:
+            if serializer.is_valid():
+                print("hi")
+                product = Product.objects.filter(Q(category__gte=19) & Q(category__lte=36))
+                serializer = ProductSerializer(product, many=True)
+                print(serializer)
+            return Response(serializer.data)
+        return Response("틀렸음")
