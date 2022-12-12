bd5fde9e2b696b07da3037aadc082f3fc417c5df
Ubuntu <ubuntu@ip-172-31-43-67.ap-northeast-2.compute.internal>
2022-12-12 09:47:57 +0000

    [수정] urls.py 수정

--- off_the_outfit/urls.py
+++ off_the_outfit/urls.py
@@ -4,14 +4,14 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('communities/', include('communities.urls')),
-    path('users/', include('users.urls')),
-    path('products/', include('products.urls')),
-    path('recommend/', include('recommend.urls')),
-    path('weather/', include('weather.urls')),
-    path('manager/', include('manager.urls')),
-    path('recommend/', include('recommend.urls')),
+    path("api/admin/", admin.site.urls),
+    path('api/communities/', include('communities.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/products/', include('products.urls')),
+    path('api/recommend/', include('recommend.urls')),
+    path('api/weather/', include('weather.urls')),
+    path('api/manager/', include('manager.urls')),
+    path('api/recommend/', include('recommend.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
