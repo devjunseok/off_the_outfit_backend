3ef83398f8eb10d38229eab28644c9c4e7c7744e
Ubuntu <ubuntu@ip-172-31-43-67.ap-northeast-2.compute.internal>
2022-12-12 12:40:54 +0000

    [수정] settings.py 수정

--- off_the_outfit/settings.py
+++ off_the_outfit/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', '0') == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend', ]
 
 
 # Application definition
