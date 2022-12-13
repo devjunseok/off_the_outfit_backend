c46e6c248391c7a5203a74f2be1e9b71abb5cfab
devjunseok <wnslgood58@naver.com>
2022-12-13 16:51:58 +0900

    [수정] schema_view 수정

--- off_the_outfit/urls.py
+++ off_the_outfit/urls.py
@@ -8,9 +8,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="offtheoutfit API",
       default_version='v1',
-      description="Test description",
+      description="offtheoutfit API",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
