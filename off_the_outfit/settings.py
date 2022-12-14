6404c415de48cea24d156cd7fa3a90c83c6aaa17
Ubuntu <ubuntu@ip-172-31-43-67.ap-northeast-2.compute.internal>
2022-12-14 10:58:06 +0000

    [수정] access token 지속시간 조정

--- off_the_outfit/settings.py
+++ off_the_outfit/settings.py
@@ -151,7 +151,7 @@ AUTH_USER_MODEL = 'users.User'
 
 
 SIMPLE_JWT = {  ##jwt 설정
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20000),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
