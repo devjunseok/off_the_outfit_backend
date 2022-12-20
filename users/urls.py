dcc6a56f806c115663b7431f6d6bf7566f6284b0
devjunseok <wnslgood58@naver.com>
2022-12-20 20:57:41 +0900

    [삭제] 불필요 코드 삭제

--- users/urls.py
+++ users/urls.py
@@ -7,11 +7,7 @@ urlpatterns = [
     path('', views.UserView.as_view(), name='user_view'),                                          # 회원가입, 회원정보 수정, 회원정보 삭제 url
     path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),       # 로그인 url
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),                  # 리프레쉬 토근 url
-    
-    path('kakao/callback/', views.KakaoLoginView.as_view(), name='kakao_callback'),# 카카오 소셜로그인 url
-    # path('kakao/login/finish/', views.KakaoLogin.as_view(), name='kakao_login_todjango'),# 카카오 소셜로그인 url
-    
-    # path('social/', views.SocialLoginView.as_view(), name='social_login_view'),
+    path('kakao/callback/', views.KakaoLoginView.as_view(), name='kakao_callback'),                # 카카오 소셜로그인 url
     path('follow/<int:user_id>/', views.FollowView.as_view(),name='follow_view'),                  # follow url
     path('passwordchange/', views.PasswordChangeView.as_view(), name='passwordchange_view'),       # 비밀번호 변경 url
     path('<int:user_id>/', views.ProfileView.as_view(), name='profile_view'),                      # 회원 정보 상세 조회, 수정 url
