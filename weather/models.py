37925a9aedd197afb53699da1388f2e16e71792a
devjunseok <wnslgood58@naver.com>
2022-12-21 17:41:38 +0900

    [수정] 날씨 DB max_length 수정

--- weather/models.py
+++ weather/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Weather(models.Model):
-    city = models.CharField('지역명', max_length=10)
-    day_date = models.CharField('날짜', max_length=10)
+    city = models.CharField('지역명', max_length=50)
+    day_date = models.CharField('날짜', max_length=50)
     day_temperature_highest = models.IntegerField('최고기온')
     day_temperature_lowest = models.IntegerField('최저기온')
     day_temperature = models.IntegerField('평균기온')
-    day_blind = models.CharField('날씨상태', max_length=10)
+    day_blind = models.CharField('날씨상태', max_length=50)
 
     def __str__(self):
         return str(self.city)
