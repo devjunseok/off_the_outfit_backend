5c2fbb361226be092f0c5a375889348ce64dd3f2
devjunseok <wnslgood58@naver.com>
2022-12-24 15:23:37 +0900

    [수정] 불필요 url 삭제

--- off_the_outfit/urls.py
+++ off_the_outfit/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path("users/", include('django.contrib.auth.urls')),
 ]
 handler404 = 'users.views.page_not_found'
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
